284043cbe99817c7e13943af49dbac6e643c1abe
Akassharjun Shanmugarajah <akassharjun@192.168.1.2>
2019-12-30 11:45:35 +0530

    30/12/2019 update.

--- js/index.js
+++ js/index.js
@@ -1,21 +1,54 @@
+let destination = '';
+let comfortLevel = '';
+let startDate = '';
+let endDate = '';
+let activity = '';
+let priceRange = '';
+
 $(function () {
-    const availableDestinations = [
-        "The Alps",
-        "Europe",
-        "Africa",
-        "The Caribbean",
-        "Indian Ocean",
-        "North America",
-        "Central America",
-        "South America",
-        "Asia",
-    ];
-    $("#destination").autocomplete({
-        source: availableDestinations
+    $("#start-date").datepicker({
+        onSelect: function (dateText) {
+            startDate = dateText;
+        }
+    });
+    $("#end-date").datepicker({
+        onSelect: function (dateText) {
+            endDate = dateText;
+        }
     });
 });
 
-$(function () {
-    $("#start-date").datepicker();
-    $("#end-date").datepicker();
+$(document).ready(function () {
+
+
+    $("select#destination").change(function () {
+        destination = $(this).children("option:selected").val();
+    });
+
+    $("select#comfort-level").change(function () {
+        comfortLevel = $(this).children("option:selected").val();
+    });
+
+    $("select#activities").change(function () {
+        activity = $(this).children("option:selected").val();
+    });
+
+    $("select#price").change(function () {
+        priceRange = $(this).children("option:selected").val();
+    });
+
+    $('#search-button').click(function () {
+        console.log(
+            `Destination : ${destination}\nComfort Level : ${comfortLevel}\n
+            Start Date : ${startDate}\nEnd Date : ${endDate}\n
+            Activity : ${activity}\nPrice Range : ${priceRange}`
+        );
+        localStorage.setItem("destination", `${destination}`);
+        localStorage.setItem("comfortLevel", `${comfortLevel}`);
+        localStorage.setItem("startDate", `${startDate}`);
+        localStorage.setItem("endDate", `${endDate}`);
+        localStorage.setItem("activity", `${activity}`);
+        localStorage.setItem("priceRange", `${priceRange}`);
+        return false;
+    });
 });
